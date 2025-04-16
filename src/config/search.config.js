9643db9f96fe22952c4f3399ea20d02cd83f2c2a
Techflashfund <Tech@flashfund.in>
2025-04-16 11:45:07 +0530

    updated

--- src/config/search.config.js
+++ src/config/search.config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    PRIVATE_KEY: '85x/NZeo6H6asmvk0dtWb32ccZBOWgdUujTGpnbZiVacJQ0/RpW1+t0199e2wL9uHDT9fqggWrPFvnzHgFagfQ==',
+    PRIVATE_KEY: 'Vc4pNqCe64hM5iFyBsh9yIZcf1cUnMfRUfI0w9hQ4TzSu0kglOFUN1CDl0QbGljWMJoisP/FvRxDayTCHq18ug==',
     API_ENDPOINTS: {
-      SEARCH: 'https://preprod.gateway.ondc.org/search',
+      SEARCH: 'https://prod.gateway.ondc.org/search',
       STORE_RESPONSE: 'https://flashfund.kitezone.in/api/store-response'
     },
-    KEY_ID: 'pl.pr.flashfund.in|58070f41-8cae-2f577-b8ca-24273d9b07b3|ed25519',
+    KEY_ID: 'personal-loan.flashfund.in|58028f41-8cae-4f577-b8ca-54273d9b07a1|ed25519',
     SIGNATURE_VALIDITY: 3600000 
   }
 
