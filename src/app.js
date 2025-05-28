a104af4e92822eba0c6ef4e26eb4a04b943722ec
Techflashfund <Tech@flashfund.in>
2025-05-28 14:06:45 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -50,7 +50,8 @@ app.use(cors({
         'http://localhost:3001',
         'https://personal-loan-571d4n33v-techflashfunds-projects.vercel.app',
         'https://personal-loan-ui.vercel.app',
-       'https://personal-loan-dashboard.vercel.app'
+       'https://personal-loan-dashboard.vercel.app',
+      'https://pl-pf-admin-dashboard.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
